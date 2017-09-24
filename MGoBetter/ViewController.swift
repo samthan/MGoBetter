02a791d070cc13b75f38e7d8861e36c8eaf95acc
Samuel Han <samthan@Samuels-MacBook-Pro.local>
2017-09-24 14:54:51 -0700

    test

---
+++ MGoBetter/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MGoBetter
+//
+//  Created by Samuel Han on 9/24/17.
+//  Copyright © 2017 Samuel Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // Mark: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Mark: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+
+    // Mark: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+    
+}
+
